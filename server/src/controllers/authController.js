09da3d945380123be3bcfbc3087270719ace669e
Hanvitha Reddy <hanvitha.chavva@gmail.com>
2026-07-17 19:31:27 +0530

    Implement user registration with MongoDB

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -2,10 +2,30 @@ const User = require("../models/User");
 
 const register = async (req, res) => {
   try {
-    res.status(200).json({
-      success: true,
-      message: "Register API working",
+    console.log("NEW CONTROLLER IS RUNNING");
+    const { fullName, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already registered",
+      });
+    }
+    const newUser = await User.create({
+      fullName,
+      email,
+      password,
     });
+    console.log(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: newUser,
+    });
+
   } catch (error) {
     res.status(500).json({
       success: false,
