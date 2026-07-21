1f58aae7290183c8c44deb7b665136f134a2b29e
Hanvitha Reddy <hanvitha.chavva@gmail.com>
2026-07-21 10:53:11 +0530

    Add interview routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const authRoutes = require("./routes/authRoutes");
+const interviewRoutes = require("./routes/interviewRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/interviews", interviewRoutes);
 
 module.exports = app;
---
+++ server/src/routes/interviewRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { createInterview } = require("../controllers/interviewController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.post("/", authMiddleware, createInterview);
+
+module.exports = router;
