4b4cbdeb88ab781a3937dd615399e4c7e48d8330
Hanvitha Reddy <hanvitha.chavva@gmail.com>
2026-07-28 18:47:15 +0530

    feat: implement React authentication setup and login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
+import Login from "./pages/Login";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-100">
-      <h1 className="text-5xl font-bold text-blue-600">
-        AI Interview Preparation Platform 🚀
-      </h1>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Handle input changes
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
+export default Login;
