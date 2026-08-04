f29b9a46721fb7207c545df7370adb6e43d8e88f
Hanvitha Reddy <hanvitha.chavva@gmail.com>
2026-08-04 20:55:17 +0530

    feat: display interview history on dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import api from "../services/api";
 function Dashboard() {
   const { user } = useAuth();
   const navigate = useNavigate();
+  const [interviews, setInterviews] = useState([]);
+  useEffect(() => {
+    fetchInterviews();
+  }, []);
 
+  const fetchInterviews = async () => {
+    try {
+      const response = await api.get("/interviews");
+
+      console.log(response.data);
+
+      setInterviews(response.data.interviews);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
       <h1>AI Interview Preparation Platform</h1>
@@ -20,7 +37,45 @@ function Dashboard() {
 
       <h3>Your Interviews</h3>
 
-      <p>No interviews found.</p>
+        {interviews.length === 0 ? (
+          <p>No interviews found.</p>
+        ) : (
+          interviews.map((interview) => (
+            <div
+              key={interview._id}
+              style={{
+                border: "1px solid #ccc",
+                padding: "15px",
+                marginBottom: "15px",
+                borderRadius: "8px",
+              }}
+            >
+              <h4>{interview.role}</h4>
+
+              <p>
+                <strong>Difficulty:</strong> {interview.difficulty}
+              </p>
+
+              <p>
+                <strong>Status:</strong> {interview.status}
+              </p>
+
+              {interview.status === "completed" ? (
+              <button
+                onClick={() => navigate(`/interview-result/${interview._id}`)}
+              >
+                View Result
+              </button>
+          ) : (
+            <button
+              onClick={() => navigate(`/interview/${interview._id}`)}
+            >
+              Continue Interview
+            </button>
+          )}
+            </div>
+          ))
+        )}
     </div>
   );
 }
